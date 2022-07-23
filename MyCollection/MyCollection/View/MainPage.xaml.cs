01996b5a96d824b4d55c75c55c548b80e6c8b0d5
Jaweria321 <jaweria.ehsan123@gmail.com>
2022-07-24 02:05:15 +0500

    Add ListView and bind with ViewModel

--- MyCollection/MyCollection/View/MainPage.xaml.cs
+++ MyCollection/MyCollection/View/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyCollection.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,9 +11,23 @@ namespace MyCollection
 {
     public partial class MainPage : ContentPage
     {
+
+        AlbumViewModel avm;
         public MainPage()
         {
             InitializeComponent();
+            avm = new AlbumViewModel();// AlbumViewModel's object
+            avm.CollectionName = "Jaweria's Collection"; // Assigning value AlbumViewModel's object
+            // Binding AlbumViewModel with MainPage
+            BindingContext = avm;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await avm.GetAlbums();
+            //AlbumListview is ListView name 
+            AlbumListview.ItemsSource = avm.AlbumList;
         }
     }
 }
---
+++ MyCollection/MyCollection/ViewModel/AlbumViewModel.cs
@@ -0,0 +1,59 @@
+﻿using MyCollection.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection.ViewModel
+{
+    class AlbumViewModel : INotifyPropertyChanged
+    {
+
+        private string _CollectionName;
+        public string CollectionName
+        {
+            get
+            {
+                return _CollectionName;
+            }
+            set
+            { 
+                _CollectionName = value;
+                RaisePropertyChanged("CollectionName");
+            }
+
+        }
+
+        // A list of custom type named AlbumModel
+        public List<AlbumModel> AlbumList;
+        public AlbumModel SelectedAlbum;
+
+        public async  Task<bool> GetAlbums()
+        {
+            if ( AlbumList == null)
+            {
+                AlbumList = new List<AlbumModel>();
+                AlbumList.Add(new AlbumModel("Volunteers", "Jefferson Airplane", "Box 1", "", "Rock"));
+                AlbumList.Add(new AlbumModel("Escape", "Journey", "Box 2", "", "Rock"));
+                AlbumList.Add(new AlbumModel("Exit", "Tangerine Dream", "Box 1", "", "Electronic"));
+                AlbumList.Add(new AlbumModel("Rumors", "Fleetwood Mac", "Box 2", "", "Rock"));
+                AlbumList.Add(new AlbumModel("LA Woman", "The Doors", "Box 1", "", "Rock"));
+                AlbumList.Add(new AlbumModel("Moving Pictures", "Rush", "Box 2", "", "Rock"));
+
+            }
+            return true;
+        }
+        
+        
+        
+        
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+    }
+}
