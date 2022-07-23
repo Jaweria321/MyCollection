26b7782391687616af37f8a96ac5b10d83c6d46e
Jaweria321 <jaweria.ehsan123@gmail.com>
2022-07-23 23:23:41 +0500

    Add project files.

---
+++ MyCollection/MyCollection/Model/AlbumModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCollection.Model
+{
+    class AlbumModel
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Location { get; set; }
+        public string Thumb { get; set; }
+        public string Genre { get; set; }
+
+        public AlbumModel(string title,string artist, string location,string thumb,string genre)
+        {
+            Title = title;  
+            Artist = artist;
+            Location = location;
+            Thumb = thumb;
+            Genre = genre;
+        }
+    }
+
+    
+}
